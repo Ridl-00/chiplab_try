3496d4992bc5f0df6f5a33ff16be7124f1fb40b0
Ridl-00 <2190585675@qq.com>
2025-03-07 17:16:23 +0800

    搭载open-la500完成58条功能测试

---
+++ fpga/nscscc-team/run_vivado/run_allbench.tcl
@@ -0,0 +1,89 @@
+#性能测试
+file copy -force ../../software/perf/obj/bitcount/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+
+file copy -force ../../software/perf/obj/bubble_sort/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+
+file copy -force ../../software/perf/obj/coremark/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+
+file copy -force ../../software/perf/obj/crc32/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+
+file copy -force ../../software/perf/obj/dhrystone/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+
+file copy -force ../../software/perf/obj/quick_sort/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+
+file copy -force ../../software/perf/obj/select_sort/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+
+file copy -force ../../software/perf/obj/sha/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+
+file copy -force ../../software/perf/obj/stream_copy/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+
+file copy -force ../../software/perf/obj/stringsearch/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/fireye_A0/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/fireye_B2/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/fireye_C0/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/fireye_D1/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/fireye_I2/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/inner_product/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/lookup_table/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/loop_induction/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/my_memcmp/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
+
+file copy -force ../../software/perf/obj/minmax_sequence/axi_ram.mif ./loongson.sim/sim_1/behav/xsim/axi_ram.mif
+restart
+run all
